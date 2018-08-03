0cb53c78f351bc919fdfc71755ef342de84229c7
tao <marius.a.nicolae@outlook.com>
2018-08-03 22:30:27 +0300

    Runnable demo

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,35 @@ public class Main {
     //We can prevent two threads to execute two instances of Singleton
     //with synchronization
 
-
-
-
+    //A deadlock is a situation where a thread T1, holds a key
+    //needed by a thread T2,
+    //and T2 holds the key needed by T1
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //The most basic way to create threads in
+        //Java is to use the Runnable Pattern
+
+        //First create an instance of Runnable
+        //Java 7
+//        Runnable runnableJava7 = new Runnable() {
+//            @Override
+//            public void run() {
+//                String nameOfthisThread = Thread.currentThread().getName();
+//                System.out.println("I'm running in thread " + nameOfthisThread);
+//            }
+//        };
+
+        //Java 8
+        Runnable runnableJava8 = () -> {
+            String nameOfthisThread = Thread.currentThread().getName();
+            System.out.println("I'm running in thread " + nameOfthisThread);
+        };
+        //Then pass it to the constructor of the Thread class
+        Thread thread = new Thread(runnableJava8);
+        //Then call the start() method of this thread object
+        thread.start();
     }
+
+
+
 }
