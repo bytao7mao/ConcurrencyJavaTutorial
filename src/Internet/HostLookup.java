b8d606f214c01271fc1c9d6b4cc9730c5070a579
tao <marius.a.nicolae@outlook.com>
2018-08-06 21:58:48 +0300

    added Internet chapter

---
+++ src/Internet/HostLookup.java
@@ -0,0 +1,39 @@
+package Internet;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+/**
+ * Created by taoLen on 8/6/2018.
+ */
+public class HostLookup {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the IP lookup application.");
+        String host;
+        do{
+            System.out.println("\nEnter a host name: ");
+            host = scanner.nextLine();
+            try {
+                InetAddress[] addresses
+                        = InetAddress.getAllByName(host);
+                for (InetAddress ip:addresses){
+                    System.out.println(ip.toString());
+                }
+            }catch (UnknownHostException e){
+                System.out.println("Unknown host.");
+            }
+        }while (doAgain());
+    }
+    private static boolean doAgain(){
+        System.out.println();
+        String s;
+        while (true){
+            System.out.println("Look up for another ?? " + "(Y or N)");
+            s = scanner.nextLine();
+            return s.equalsIgnoreCase("y");
+        }
+    }
+}
