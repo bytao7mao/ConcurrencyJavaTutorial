e6daac588f7e0b430b27958a5fc782b09d579d88
tao <marius.a.nicolae@outlook.com>
2018-08-08 19:56:51 +0300

    updated some reflection methods

--- src/Volatile/Classing.java
+++ src/Volatile/Classing.java
@@ -1,18 +1,44 @@
 package Volatile;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 /**
  * Created by taoLen on 8/4/2018.
  */
 public class Classing {
-    volatile static int index;
-    static void increment(){index++;}
-    static void print(){
+    public volatile static int index=0;
+    public static void increment(){index++;}
+    public static void print(){
         System.out.println(index);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
         print();
         increment();
         print();
+        Classing classing = new Classing();
+        for(Field f : classing.getClass().getFields()) {
+            System.out.println(f.getGenericType() +" - "+f.getName() + " = " + f.get(classing));
+        }
+        System.out.println("The public methods of class are: ");
+        // Getting methods of the class through the object
+        // of the class by using getMethods
+        Class cls = classing.getClass();
+        Method[] methods = cls.getMethods();
+        // Printing method names
+        for (Method method:methods){
+            if (method.getName().equalsIgnoreCase("wait")||
+                    method.getName().equalsIgnoreCase("equals")||
+                    method.getName().equalsIgnoreCase("toString")||
+                    method.getName().equalsIgnoreCase("hashcode")||
+                    method.getName().equalsIgnoreCase("notify")||
+                    method.getName().equalsIgnoreCase("notifyall")||
+                    method.getName().equalsIgnoreCase("getClass")){
+                return;
+            }else {
+                System.out.print(method.getName()+" ");
+            }
+        }
     }
 }
