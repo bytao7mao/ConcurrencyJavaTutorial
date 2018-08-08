d336cf16aaf3a44be27c1fb45fac42a5333223ad
tao <marius.a.nicolae@outlook.com>
2018-08-08 20:41:19 +0300

    added other test cases for getting methods

--- src/Volatile/Classing.java
+++ src/Volatile/Classing.java
@@ -7,10 +7,26 @@ import java.lang.reflect.Method;
  * Created by taoLen on 8/4/2018.
  */
 public class Classing {
-    public volatile static int index=0;
-    public static void increment(){index++;}
+    private volatile static int index=0;
+    public static final String wait = "wait";
+    private static final String equals = "equals";
+    private static final String toString = "toString";
+    private static final String hashcode = "hashCode";
+    private static final String notify = "notify";
+    private static final String notifyall = "notifyAll";
+    private static final String getclass = "getClass";
+
+    public static void increment(){
+        setIndex(getIndex() + 1);
+    }
     public static void print(){
-        System.out.println(index);
+        System.out.println(getIndex());
+    }
+    public static int getIndex() {
+        return index;
+    }
+    public static void setIndex(int index) {
+        Classing.index = index;
     }
 
     public static void main(String[] args) throws IllegalAccessException {
@@ -26,19 +42,72 @@ public class Classing {
         // of the class by using getMethods
         Class cls = classing.getClass();
         Method[] methods = cls.getMethods();
+
         // Printing method names
         for (Method method:methods){
-            if (method.getName().equalsIgnoreCase("wait")||
-                    method.getName().equalsIgnoreCase("equals")||
-                    method.getName().equalsIgnoreCase("toString")||
-                    method.getName().equalsIgnoreCase("hashcode")||
-                    method.getName().equalsIgnoreCase("notify")||
-                    method.getName().equalsIgnoreCase("notifyall")||
-                    method.getName().equalsIgnoreCase("getClass")){
-                return;
-            }else {
-                System.out.print(method.getName()+" ");
+            switch (method.getName()){
+                case wait:
+                case equals:
+                case toString:
+                case hashcode:
+                case notify:
+                case notifyall:
+                case getclass:
+                    return;
+                default:
+                    System.out.print(method.getName()+" ");
             }
+//            if (method.getName().equalsIgnoreCase("wait")||
+//                    method.getName().equalsIgnoreCase("equals")||
+//                    method.getName().equalsIgnoreCase("toString")||
+//                    method.getName().equalsIgnoreCase("hashcode")||
+//                    method.getName().equalsIgnoreCase("notify")||
+//                    method.getName().equalsIgnoreCase("notifyall")||
+//                    method.getName().equalsIgnoreCase("getClass")){
+//                return;
+//            }else {
+//                System.out.print(method.getName()+" ");
+//            }
         }
+    }
+
+//    public String getWait() {
+//        return wait;
+//    }
+//
+//    public String getEquals() {
+//        return equals;
+//    }
+//
+//    public String getToString() {
+//        return toString;
+//    }
+//
+//    public String getHashcode() {
+//        return hashcode;
+//    }
+//
+//    public String getNotify() {
+//        return notify;
+//    }
+//
+//    public String getNotifyall() {
+//        return notifyall;
+//    }
+//
+//    public String getGetclass() {
+//        return getclass;
+//    }
+
+    public enum Methods {
+        wait{
+            public String toString(){
+                return "wait";
+            }
+        },equals{
+            public String toString(){
+                return "equals";
+            }
+        },toString,hashcode,notify,notifyall,getclass;
     }
 }
